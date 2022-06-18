f9be16601052e9dc150f136e5fb47d2cd092ddf0
Davi Garcia <davigs@hotmail.com>
2022-06-17 22:51:33 -0300

    add redux test

--- redux-test/src/App.js
+++ redux-test/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import './App.css'
-import DataDetail from './components/DataDetail'
-import DataList from "./components/DataList"
+import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actionCreators from "./store/actionCreators";
 
-export default function App() {
-    return (
-        <div className="App">
-            <DataList/>
-            <DataDetail/>
-        </div>
-    )
+function App() {
+  const account = useSelector((state) => state.account)
+  const dispatch = useDispatch()
+
+  const { deposit, withdraw } = bindActionCreators(actionCreators, dispatch)
+
+  return (
+    <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => deposit(100)}> Deposit </button>
+      <button onClick={() => withdraw(100)}> Withdraw </button>
+    </div>
+  );
 }
+
+export default App;
